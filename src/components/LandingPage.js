867a9f325f211501dbef803083d2780c55b19bf0
JoaNattapon <joenattapon.sangchai@gmail.com>
2023-01-17 08:15:22 +0700

    first initialize

---
+++ src/components/LandingPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './style.css';
+
+const LandingPage = () => {
+
+    return (
+        <div className="landing-header">
+            <h2>Hello Final Project !</h2>
+        </div>
+    )
+}
+
+export default LandingPage;
