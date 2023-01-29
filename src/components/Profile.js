ed9b0ccd682c65618fafd132fb99314e0c66823f
JoaNattapon <joenattapon.sangchai@gmail.com>
2023-01-29 10:46:28 +0700

    reverse label tag

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -66,34 +66,21 @@ const Profile = () => {
                     <div>
                         <h2>{user.name}Profile</h2>
                         <form>
-                            <label>
-                                Username :
+                            
                                 <input className="signupbtn" type="text" value={user.username} defaultValue={res.data.username} onChange={(e) => { setUsername(e.target.value) }} />
-                            </label>
-                            <label>
-                                Firstname :
+                            
                                 <input className="signupbtn" type="text" value={user.firstname} defaultValue={res.data.firstname} onChange={(e) => { setFirstname(e.target.value) }} />
-                            </label>
-                            <label>
-                                Lastname :
+                            
                                 <input className="signupbtn" type="text" value={user.lastname} defaultValue={res.data.lastname} onChange={(e) => { setLastname(e.target.value) }} />
-                            </label>
-                            <label>
-                                Phone :
+                            
                                 <input className="signupbtn" type="text" value={user.phone} defaultValue={res.data.phone} onChange={(e) => { setPhone(e.target.value) }} />
-                            </label>
-                            <label>
-                                Address :
+                            
                                 <input className="signupbtn" type="text" value={user.address} defaultValue={res.data.address} onChange={(e) => { setAddress(e.target.value) }} />
-                            </label>
-                            <label>
-                                Email :
+                            
                                 <input className="signupbtn" type="text" value={user.email} defaultValue={res.data.email} onChange={(e) => { setEmail(e.target.value) }} />
-                            </label>
-                            <label>
-                                Package bought :
+                            
                                 <input className="signupbtn" type="text" value={user.mypackage} defaultValue={res.data.mypackage} onChange={(e) => { setMyPackage(e.target.value) }} />
-                            </label>
+                            
                         </form>
                         <button className="signupclick" onClick={() => edit()}>Confirm</button>
                     </div>
